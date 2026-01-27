08a105bf0753862ee588eec965f9517dcef283d6
AsimMahata <asimmahata02@gmail.com>
2026-01-27 17:32:04 +0530

    add lsp support

---
+++ README.md
@@ -0,0 +1,25 @@
+# **Nice**
+
+* **N**ice **I**s a **C**ode **E**ditor which mainly focuses on
+competitive programming
+
+## Status
+
+**Currently under active development**
+
+## Frontend
+
+* Built with **React** and **TypeScript**
+* Runs on **Tauri** for a lightweight desktop experience
+
+## Backend
+
+* Powered by **Node.js**
+* Uses **Express** for the application server
+* Uses **WebSocket** to communicate with LSP
+
+## How to Use
+
+* this section will be updated later 🥲
+
+---
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";  
+import React from "react";
 import { useState, useMemo, useEffect } from 'react'
 import CodeEditor from '../components/CodeEditor'
 import EditorControls from '../components/EditorControls'
@@ -13,83 +13,83 @@ import { getPlaceholder } from '../utils/getPlaceholder'
 
 
 function Home() {
-  const [lang, _setLang] = useState<string>('java')                   // selected Compiler
-  const [codeFile, setCodeFile] = useState<FileEntry | null>(null)   // the file that is opned
-  const [output, setOutput] = useState('No Output')                  // the output panel
-  const [error, setError] = useState('')                             // the error panel
-  const [running, setRunning] = useState(false)                      // whether the code is currently running
-  const [aiResponse, setAiResponse] = useState('')                   // the aiResponse content
-  const [input, setInput] = useState('')                             // input panel content
-  const [savingCode, setSavingCode] = useState<boolean>(false);      // its a toogle that triggers save code function
-  const placeholder = useMemo(() => {
-      return getPlaceholder(lang);
-  }, [lang]);
-  const [code, setCode] = useState<string>(placeholder);           // the main code inside editor-area
+    const [lang, _setLang] = useState<string>('java')                   // selected Compiler
+    const [codeFile, setCodeFile] = useState<FileEntry | null>(null)   // the file that is opned
+    const [output, setOutput] = useState('No Output')                  // the output panel
+    const [error, setError] = useState('')                             // the error panel
+    const [running, setRunning] = useState(false)                      // whether the code is currently running
+    const [aiResponse, setAiResponse] = useState('')                   // the aiResponse content
+    const [input, setInput] = useState('')                             // input panel content
+    const [savingCode, setSavingCode] = useState<boolean>(false);      // its a toogle that triggers save code function
+    const placeholder = useMemo(() => {
+        return getPlaceholder(lang);
+    }, [lang]);
+    const [code, setCode] = useState<string>(placeholder);           // the main code inside editor-area
 
-  useEffect(() => {
-    if (code !== placeholder) {
-      setCode(placeholder);
-    }
-  }, [lang]);
+    useEffect(() => {
+        if (code !== placeholder) {
+            setCode(placeholder);
+        }
+    }, [lang]);
 
 
 
-  return (
-    <div className="app-root">
-      {/* This is the Top Pannel with all the buttons such as Run, Compile , Ai and mainly control all the things for now */}
-      <EditorControls
-        code={code}
-        output={output}
-        setOutput={setOutput}
-        error={error}
-        setError={setError}
-        setRunning={setRunning}
-        setAiResponse={setAiResponse}
-        input={input}
-        setSavingCode={setSavingCode}
-        lang={lang}
-        _setLang={_setLang}
-      />
-
-      {/*rest of the things such as file-explorer code-area input output error aiResponse etc..*/}
-      <div className="workspace">
-        {/* File Explorer */}
-        <aside className="file-explorer">
-          <FileEx
-            codeFile={codeFile}
-            setCodeFile={setCodeFile}
-            code={code}
-            setCode={setCode}
-            savingCode={savingCode}
-            setSavingCode={setSavingCode}
-          />
-        </aside>
-
-        {/* Main Editor Area */}
-        <main className="editor-area">
-          <CodeEditor code={code} setCode={setCode} lang={lang} />
-
-          <div className="panels">
-            <InputPanel input={input} setInput={setInput} />
-
-            <OutputPannel
-              output={output}
-              setOutput={setOutput}
-              running={running}
+    return (
+        <div className="app-root">
+            {/* This is the Top Pannel with all the buttons such as Run, Compile , Ai and mainly control all the things for now */}
+            <EditorControls
+                code={code}
+                output={output}
+                setOutput={setOutput}
+                error={error}
+                setError={setError}
+                setRunning={setRunning}
+                setAiResponse={setAiResponse}
+                input={input}
+                setSavingCode={setSavingCode}
+                lang={lang}
+                _setLang={_setLang}
             />
 
-            <ErrorPannel
-              error={error}
-              setError={setError}
-              running={running}
-            />
+            {/*rest of the things such as file-explorer code-area input output error aiResponse etc..*/}
+            <div className="workspace" >
+                {/* File Explorer */}
+                <aside className="file-explorer">
+                    <FileEx
+                        codeFile={codeFile}
+                        setCodeFile={setCodeFile}
+                        code={code}
+                        setCode={setCode}
+                        savingCode={savingCode}
+                        setSavingCode={setSavingCode}
+                    />
+                </aside>
 
-            <AiHelpPanel aiResponse={aiResponse} />
-          </div>
-        </main>
-      </div>
-    </div>
-  )
+                {/* Main Editor Area */}
+                <main className="editor-area">
+                    <CodeEditor key={lang} code={code} setCode={setCode} lang={lang} />
+
+                    <div className="panels">
+                        <InputPanel input={input} setInput={setInput} />
+
+                        <OutputPannel
+                            output={output}
+                            setOutput={setOutput}
+                            running={running}
+                        />
+
+                        <ErrorPannel
+                            error={error}
+                            setError={setError}
+                            running={running}
+                        />
+
+                        <AiHelpPanel aiResponse={aiResponse} />
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
